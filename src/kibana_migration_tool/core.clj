477760f4de2d37f07feab20f772c795a04d0e425
Ivan Sharamet <isharamet@gmail.com>
2019-04-07 14:37:48 +0300

    Implement simple import function

--- src/kibana_migration_tool/core.clj
+++ src/kibana_migration_tool/core.clj
@@ -1,5 +1,7 @@
 (ns kibana-migration-tool.core
-  (:require [clojure.java.io :as io]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :refer [join]]
             [clj-http.client :as client]
             [cheshire.core :refer :all])
   (:gen-class))
@@ -19,30 +21,61 @@
 (defn doc-type
   "Extracts document type (i.e. dashboard/visualization/config etc.)."
   [doc]
-  (keyword (get-in doc [:_source :type])))
+  (get-in doc [:_source :type]))
 
 (defn save
-  "Saves collection of documents in `dst` grouping them by type."
+  "Saves collection of documents in `dst` directory grouping them by type."
   [docs dst]
-  (doseq [[t ds] (group-by doc-type docs)
-          d ds]
-    (let [p (io/file (str dst "/" t))
-          id (:_id d)
-          f (str p "/" id ".edn")]
-      (do (.mkdirs p)
-          (spit f d)))))
+  (doseq [[type docs] (group-by doc-type docs)
+          doc docs]
+    (let [path (io/file (str dst "/" type))
+          id (:_id doc)
+          fname (str path "/" id ".edn")]
+      (do (.mkdirs path)
+          (spit fname doc)))))
 
 (defn kibana-export
   "Exports all documents from `scr` .kibana index and saves them in
-  specified `dst` location."
+  specified `dst` directory."
   [src dst]
-  (let [docs (get-docs src)]
+  (let [docs (kibana-docs src)]
     (save docs dst)))
 
-(defn kibana-import [src dst]
-  ())
+(defn read-docs
+  "Returns lazy sequence with all the documents in `src` directory."
+  [src]
+  (->> (io/file src)
+       (file-seq)
+       (filter #(.isFile %))
+       (map slurp)
+       (map edn/read-string)))
 
-(kibana-export "http://localhost:9200" "./backup")
+(defn to-bulk-update
+  "Transforms document collection to Elasticsearch bulk update body."
+  [docs]
+  (->> docs
+       (mapcat (fn [doc]
+                 [{:update {:_id (:_id doc)
+                            :_type "doc"
+                            :_index ".kibana"}}
+                  {:doc (:_source doc)
+                   :doc_as_upsert true}]))
+       (map generate-string)
+       (join "\n")))
+
+(defn upsert-docs
+  "Updates or inserts provided documents into `dst` Elasticsearch instance."
+  [dst docs]
+  (let [body (to-bulk-update docs)
+        rs (client/post (str dst "/_bulk")
+                        {:body (str body "\n")
+                         :content-type :json
+                         :accept :json})]
+    rs))
+
+(defn kibana-import [src dst]
+  (let [docs (read-docs src)]
+    (upsert-docs dst docs)))
 
 (defn -main []
-  (println kibana))
+  (println "It works!"))
