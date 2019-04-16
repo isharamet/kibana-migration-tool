768e876d013e0f0992754e31d46ff1175b79b0d2
Ivan Sharamet <isharamet@gmail.com>
2019-04-16 14:57:12 +0300

    Add more unit tests

--- test/kibana_migration_tool/test/core.clj
+++ test/kibana_migration_tool/test/core.clj
@@ -2,14 +2,35 @@
   (:require [clojure.test :as test :refer :all]
             [kibana-migration-tool.core :as c]))
 
+(deftest add-auth-test
+  (let [rq {}]
+    (testing "without auth"
+      (let [auth nil
+            expected-rq rq
+            actual-rq (c/add-auth rq auth)]
+        (is (= actual-rq expected-rq))))
+
+    (testing "with unsupported auth type"
+      (let [auth {:type :digest :auth "user:password"}
+            expected-rq rq
+            actual-rq (c/add-auth rq auth)]
+        (is (= actual-rq expected-rq))))
+    
+    (testing "with supported auth type"
+      (let [auth {:type :basic :auth "user:password"}
+            expected-rq (assoc rq :basic-auth "user:password")
+            actual-rq (c/add-auth rq auth)]
+        (is (= actual-rq expected-rq))))))
+
 (deftest export-request-test
   (let [export-rq {:query-params {:size 10000}
                    :body "{\"query\":{\"match_all\":{}}}"
                    :content-type :json
                    :accept :json}]
     (testing "without auth"
-      (let [expected-rq export-rq
-            actual-rq (c/export-request nil)]
+      (let [auth nil
+            expected-rq export-rq
+            actual-rq (c/export-request auth)]
         (is (= actual-rq expected-rq))))
 
     (testing "with auth"
@@ -18,3 +39,20 @@
             actual-rq (c/export-request auth)]
         (is (= actual-rq expected-rq))))))
 
+(deftest import-request-test
+  (let [import-rq {:body "\n"
+                   :content-type :json
+                   :accept :json}]
+    (testing "without auth"
+      (let [docs []
+            auth nil
+            expected-rq import-rq
+            actual-rq (c/import-request docs auth)]
+        (is (= actual-rq expected-rq))))
+    
+    (testing "with auth"
+      (let [docs []
+            auth {:type :basic :auth "user:password"}
+            expected-rq (assoc import-rq :basic-auth "user:password")
+            actual-rq (c/import-request docs auth)]
+        (is (= actual-rq expected-rq))))))
